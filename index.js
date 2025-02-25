11c04f5f452eea2361a5d4ec37abf9a21ff8c8c7
sahil <sahilsinghnayal@gmail.com>
2025-02-25 20:33:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const fs = require("fs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public"))); //  sari static file yhain milegi
+app.set("view engine", "ejs"); // Set the view engine to ejs
+
+app.get("/", (req, res) => {
+  fs.readdir("./files", (err, files) => {
+    console.log(files);
+    res.render("index",{files:files});
+  });
+
+});
+app.post("/create", (req, res) => {
+  console.log(req.body);
+  
+  fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`, req.body.details, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect("/");
+  });
+
+
+
+
+
+});
+
+app.listen(3000);

59487e835415bc8c3928866c563a9faec89ae861
sahil <sahilsinghnayal@gmail.com>
2025-02-25 21:37:30 +0530

    how to view file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const fs = require("fs");
+const { log } = require("console");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public"))); //  sari static file yhain milegi
@@ -9,13 +10,22 @@ app.set("view engine", "ejs"); // Set the view engine to ejs
 
 app.get("/", (req, res) => {
   fs.readdir("./files", (err, files) => {
-    console.log(files);
+    // console.log(files);
     res.render("index",{files:files});
   });
 
 });
+app.get("/files/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf8", (err, data) => {
+    
+   res.render("show",{filename:req.params.filename, data:data});
+   console.log(data);
+   
+  }); 
+
+});
 app.post("/create", (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
   
   fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`, req.body.details, (err) => {
     if (err) {
